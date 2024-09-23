e0d35bf853628f68f7c1243e3aeb3dd9b24c0355
cdizon239 <cdizon239@gmai.com>
2024-09-23 01:52:56 -0400

    style fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <h2>Questions?</h2>
+      <h2 className='footer-title'>Questions?</h2>
       <p>Call or text us:</p>
       <p>Thai: +1 (267) 221-2876</p>
       <p>Coleen: +1 (617) 991-0239</p>
