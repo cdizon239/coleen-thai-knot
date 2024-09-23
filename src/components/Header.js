864df7aa95452bb6f5f266cc40bc2f46ad352668
cdizon239 <cdizon239@gmai.com>
2024-09-23 14:24:46 -0400

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header className="header-container">
       <div className="header-content">
+        <img src='/white_logo.png' alt="Wedding Logo" className="header-logo" />
         <h1>Coleen & Thai</h1>
         <h2>Say I Do in Boston</h2>
         <p>10.19.2024</p>
